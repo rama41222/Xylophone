dbf9faea076b98bcf8af6da0097b9e593592ccf3
rama41222 <dinushankanrg@gmail.com>
2018-03-28 11:52:38 +0530

    bug fixes, removed xml on clicks, added click listeners

--- app/src/main/java/cloud/viyana/xylomusic/MainActivity.java
+++ app/src/main/java/cloud/viyana/xylomusic/MainActivity.java
@@ -2,9 +2,11 @@ package cloud.viyana.xylomusic;
 
 import android.media.AudioManager;
 import android.media.SoundPool;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,51 +26,89 @@ public class MainActivity extends AppCompatActivity {
     private int mASoundId;
     private int mBSoundId;
 
+    Button playCBtn;
+    Button playDBtn;
+    Button playEBtn;
+    Button playFBtn;
+    Button playGBtn;
+    Button playABtn;
+    Button playBBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSoundPool = new SoundPool(NR_OF_SIMULTANEOUS_SOUNDS, AudioManager.STREAM_MUSIC, 0);
+        playCBtn = (Button) findViewById(R.id.btn_c);
+        playDBtn = (Button) findViewById(R.id.btn_d);
+        playEBtn = (Button) findViewById(R.id.btn_e);
+        playFBtn = (Button) findViewById(R.id.btn_f);
+        playGBtn = (Button) findViewById(R.id.btn_g);
+        playABtn = (Button) findViewById(R.id.btn_a);
+        playBBtn = (Button) findViewById(R.id.btn_b);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            mSoundPool = new SoundPool.Builder().setMaxStreams(NR_OF_SIMULTANEOUS_SOUNDS).build();
+        } else {
+            mSoundPool = new SoundPool(NR_OF_SIMULTANEOUS_SOUNDS, AudioManager.STREAM_MUSIC, 0);
+        }
+
         mCSoundId = mSoundPool.load(getApplicationContext(), R.raw.note1_c, 1);
-        mCSoundId = mSoundPool.load(getApplicationContext(), R.raw.note2_d, 1);
-        mCSoundId = mSoundPool.load(getApplicationContext(), R.raw.note3_e, 1);
-        mCSoundId = mSoundPool.load(getApplicationContext(), R.raw.note4_f, 1);
-        mCSoundId = mSoundPool.load(getApplicationContext(), R.raw.note5_g, 1);
-        mCSoundId = mSoundPool.load(getApplicationContext(), R.raw.note6_a, 1);
-        mCSoundId = mSoundPool.load(getApplicationContext(), R.raw.note7_b, 1);
+        mDSoundId = mSoundPool.load(getApplicationContext(), R.raw.note2_d, 1);
+        mESoundId = mSoundPool.load(getApplicationContext(), R.raw.note3_e, 1);
+        mFSoundId = mSoundPool.load(getApplicationContext(), R.raw.note4_f, 1);
+        mGSoundId = mSoundPool.load(getApplicationContext(), R.raw.note5_g, 1);
+        mASoundId = mSoundPool.load(getApplicationContext(), R.raw.note6_a, 1);
+        mBSoundId = mSoundPool.load(getApplicationContext(), R.raw.note7_b, 1);
+
+
+        playCBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSoundPool.play(mCSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+            }
+        });
+
+        playDBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSoundPool.play(mDSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+            }
+        });
+
+        playEBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSoundPool.play(mESoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+            }
+        });
+
+        playFBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSoundPool.play(mFSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+            }
+        });
+
+        playGBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSoundPool.play(mGSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+            }
+        });
+
+        playABtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSoundPool.play(mASoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+            }
+        });
+
+        playBBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSoundPool.play(mBSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+            }
+        });
     }
-
-    private void playC(View v){
-        mSoundPool.play(mCSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
-
-    }
-
-    private void playD(View v){
-        mSoundPool.play(mDSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
-
-    }
-
-    private void playE(View v){
-        mSoundPool.play(mESoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
-    }
-
-    private void playF(View v){
-        mSoundPool.play(mFSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
-    }
-
-    private void playG(View v){
-        mSoundPool.play(mGSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
-    }
-
-    private void playA(View v){
-        mSoundPool.play(mASoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
-    }
-
-    private void playB(View v){
-        mSoundPool.play(mBSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
-    }
-
-
-
 }
